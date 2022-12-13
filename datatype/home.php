be740cd988f5fd113f9dd22181264fb7d805f2bf
Arham Mirza <102862412+arham10jul@users.noreply.github.com>
2022-12-13 18:43:22 +0500

    Add files via upload

---
+++ datatype/home.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+<?php
+//inc /dec
+//pre /post
+
+// $a=1;
+
+
+// $a++;
+
+// echo $a;
+
+
+
+
+
+
+//compound assign
+// $a=1000;
+// $a+=500;
+// $a=500;
+// echo $a;
+
+
+
+//logical operator
+
+
+var_dump(10==10 && 10==11);
+echo "<br>";
+var_dump(10==10 || 10==11);
+echo "<br>";
+var_dump(!(10==10));
+echo "<br>";
+?>
---
+++ datatype/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+<?php
+$id=101;
+$name="Arham";
+$gender='m';
+$status=true;
+
+var_dump($id);
+echo "<br>";
+
+var_dump($name);
+echo "<br>";
+
+var_dump($gender);
+echo "<br>";
+
+var_dump($status);
+echo "<br>"
+?>
