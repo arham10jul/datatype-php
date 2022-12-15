abfebc524bfbe6a4be594ab10a6043d9c8d3e643
Arham Mirza <102862412+arham10jul@users.noreply.github.com>
2022-12-15 18:16:45 +0500

    Add files via upload

---
+++ home.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+<?php
+//inc /dec
+//pre /post
+
+// $a=1;
+
+
+// $a++;
+
+// echo $a;
+
+
+
+
+
+
+//compound assign
+// $a=1000;
+// $a+=500;
+// $a=500;
+// echo $a;
+
+
+
+//logical operator
+
+
+// var_dump(10==10 && 10==11);
+// echo "<br>";
+// var_dump(10==10 || 10==11);
+// echo "<br>";
+// var_dump(!(10==10));
+// echo "<br>";
+
+
+//relational operator
+// $a="6";
+// $b="6";
+
+// $c=$a<$b;
+// $c=$a<=$b;
+// $c=$a>$b;
+// $c=$a>=$b;
+// $c=$a==$b;
+// $c=$a!=$b;
+// $c=$a===$b;
+
+// $c=$a!==$b;
+// echo $c;
+// var_dump($c);
+
+
+// airthmatic operator
+// $a=10;
+// $b=6;
+// $c=$a+$b;
+// $c=$a-$b;
+// $c-$a/$b;
+// $c=$a*$b;
+
+
+// $c=$a%$b;
+// echo$c;
+
+
+
+?>
